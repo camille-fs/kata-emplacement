35f55b3c8eb27637fc19a0f377a174f3d444fb5e
camillegitSS <4487021+camille75@users.noreply.github.com>
2022-02-10 09:52:08 +0100

    build(init): init maven java springboot project & dependencies

---
+++ src/main/java/fr/camss/kata/emplacement/KataEmplacementApplication.java
@@ -0,0 +1,13 @@
+package fr.camss.kata.emplacement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataEmplacementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataEmplacementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/camss/kata/emplacement/KataEmplacementApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.camss.kata.emplacement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataEmplacementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
